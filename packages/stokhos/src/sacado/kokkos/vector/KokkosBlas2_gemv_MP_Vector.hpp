f7c2e2fd3aaa88b1268b353e1c506f0218491e5a
kliegeois <kimliegeois@ymail.com>
2020-04-03 19:02:45 +0200

    Stokhos: update MP vector GEMV

--- packages/stokhos/src/sacado/kokkos/vector/KokkosBlas2_gemv_MP_Vector.hpp
+++ packages/stokhos/src/sacado/kokkos/vector/KokkosBlas2_gemv_MP_Vector.hpp
@@ -59,125 +59,133 @@
 #define Sacado_MP_Vector_GEMV_Tile_Size(size) (STOKHOS_GEMV_CACHE_SIZE / size)
 
 // Functor for the update
-template<class AViewType,
-         class XViewType,
-         class YViewType,
-         class IndexType = typename AViewType::size_type>
-struct updateF {
-  using AlphaCoeffType = typename AViewType::non_const_value_type;
-  using BetaCoeffType  = typename YViewType::non_const_value_type;
+template <class AViewType,
+          class XViewType,
+          class YViewType,
+          class IndexType = typename AViewType::size_type>
+struct updateF
+{
+    using AlphaCoeffType = typename AViewType::non_const_value_type;
+    using BetaCoeffType = typename YViewType::non_const_value_type;
 
-  updateF (const AlphaCoeffType& alpha,
-                               const AViewType& A,
-                               const XViewType& x,
-                               const BetaCoeffType& beta,
-                               const YViewType& y,
-                               const IndexType m_c) :
-    alpha_ (alpha), A_ (A), x_ (x), beta_ (beta), y_ (y), m_c_ (m_c)
-    {}
+    updateF(const AlphaCoeffType &alpha,
+            const AViewType &A,
+            const XViewType &x,
+            const BetaCoeffType &beta,
+            const YViewType &y,
+            const IndexType m_c) : alpha_(alpha), A_(A), x_(x), beta_(beta), y_(y), m_c_(m_c)
+    {
+    }
 
 public:
-  // i_tile is the current tile of the input matrix A
-  KOKKOS_INLINE_FUNCTION void
-  operator () (const IndexType& i_tile) const
-  {
-    const IndexType m = y_.extent(0);
-    const IndexType n = x_.extent(0);
+    // i_tile is the current tile of the input matrix A
+    KOKKOS_INLINE_FUNCTION void
+    operator()(const IndexType &i_tile) const
+    {
+        const IndexType m = y_.extent(0);
+        const IndexType n = x_.extent(0);
 
-    IndexType i_min = m_c_ * i_tile;
-    bool last_tile = (i_min + m_c_ >= m);
-    IndexType i_max = (last_tile) ? m : (i_min + m_c_);
+        IndexType i_min = m_c_ * i_tile;
+        bool last_tile = (i_min + m_c_ >= m);
+        IndexType i_max = (last_tile) ? m : (i_min + m_c_);
 
 #ifdef STOKHOS_HAVE_PRAGMA_UNROLL
-    #pragma unroll
+#pragma unroll
 #endif
-    for (IndexType i = i_min; i < i_max; ++i)
-      y_(i) *= beta_;
+        for (IndexType i = i_min; i < i_max; ++i)
+            y_(i) *= beta_;
 
-    for (IndexType j = 0; j < n; ++j) {
-      AlphaCoeffType alphab = alpha_ * x_(j);
+        for (IndexType j = 0; j < n; ++j)
+        {
+            AlphaCoeffType alphab = alpha_ * x_(j);
 
-      for (IndexType i = 0; i < i_max; ++i)
-        y_(i_min+i) += alphab * A_(i_min+i, j);
+            for (IndexType i = i_min; i < i_max; ++i)
+                y_(i) += alphab * A_(i, j);
+        }
     }
-  }
 
 private:
-  AlphaCoeffType alpha_;
-  typename AViewType::const_type A_;
-  typename XViewType::const_type x_;
-  BetaCoeffType beta_;
-  YViewType y_;
-  const IndexType m_c_;
+    AlphaCoeffType alpha_;
+    typename AViewType::const_type A_;
+    typename XViewType::const_type x_;
+    BetaCoeffType beta_;
+    YViewType y_;
+    const IndexType m_c_;
 };
 
 // Functor for the inner products
-template<class AViewType,
-         class XViewType,
-         class YViewType,
-         class IndexType = typename AViewType::size_type>
-struct innerF {
-  using execution_space = typename AViewType::execution_space;
-  using policy_type = Kokkos::TeamPolicy<execution_space>;
-  using member_type = typename policy_type::member_type;
+template <class AViewType,
+          class XViewType,
+          class YViewType,
+          class IndexType = typename AViewType::size_type>
+struct innerF
+{
+    using execution_space = typename AViewType::execution_space;
+    using policy_type = Kokkos::TeamPolicy<execution_space>;
+    using member_type = typename policy_type::member_type;
 
+    using AlphaCoeffType = typename AViewType::non_const_value_type;
+    using BetaCoeffType = typename YViewType::non_const_value_type;
+    using Scalar = AlphaCoeffType;
 
-  using AlphaCoeffType = typename AViewType::non_const_value_type;
-  using BetaCoeffType  = typename YViewType::non_const_value_type;
-  using Scalar = AlphaCoeffType;
-
-  innerF (const AlphaCoeffType& alpha,
-                               const AViewType& A,
-                               const XViewType& x,
-                               const BetaCoeffType& beta,
-                               const YViewType& y,
-                               const IndexType n_c) :
-    alpha_ (alpha), A_ (A), x_ (x), beta_ (beta), y_ (y), n_c_ (n_c)
-    {}
+    innerF(const AlphaCoeffType &alpha,
+           const AViewType &A,
+           const XViewType &x,
+           const BetaCoeffType &beta,
+           const YViewType &y,
+           const IndexType n_c) : alpha_(alpha), A_(A), x_(x), beta_(beta), y_(y), n_c_(n_c)
+    {
+    }
 
 public:
-  KOKKOS_INLINE_FUNCTION void
-  operator () (const member_type & team) const
-  {
-    const IndexType m = y_.extent(0);
-    const IndexType n = x_.extent(0);
-
-    const int j = team.league_rank();
-    const IndexType j_min = n_c_ * j;
-    const IndexType nj = (j_min + n_c_ > n) ? (n - j_min) : n_c_;
-    const IndexType i_min = j % m;
-
-    for (IndexType i = i_min; i < m; ++i)
+    KOKKOS_INLINE_FUNCTION void
+    operator()(const member_type &team) const
     {
-      Scalar tmp = 0.;
-      Kokkos::parallel_reduce(Kokkos::TeamThreadRange(team, nj), [=](int jj, Scalar &tmp_sum) {
-        tmp_sum += A_(jj + j_min, i)*x_(jj + j_min);
-      }, tmp);
-      if (team.team_rank() == 0) {
-        tmp *= alpha_;
-        Kokkos::atomic_add<Scalar>(&y_(i), tmp);
-      }
+        const IndexType m = y_.extent(0);
+        const IndexType n = x_.extent(0);
+
+        const int j = team.league_rank();
+        const IndexType j_min = n_c_ * j;
+        const IndexType nj = (j_min + n_c_ > n) ? (n - j_min) : n_c_;
+        const IndexType i_min = j % m;
+
+        for (IndexType i = i_min; i < m; ++i)
+        {
+            Scalar tmp = 0.;
+            Kokkos::parallel_reduce(
+                Kokkos::TeamThreadRange(team, nj), [=](int jj, Scalar &tmp_sum) {
+                    tmp_sum += A_(jj + j_min, i) * x_(jj + j_min);
+                },
+                tmp);
+            if (team.team_rank() == 0)
+            {
+                tmp *= alpha_;
+                Kokkos::atomic_add<Scalar>(&y_(i), tmp);
+            }
+        }
+        for (IndexType i = 0; i < i_min; ++i)
+        {
+            Scalar tmp = 0.;
+            Kokkos::parallel_reduce(
+                Kokkos::TeamThreadRange(team, nj), [=](int jj, Scalar &tmp_sum) {
+                    tmp_sum += A_(jj + j_min, i) * x_(jj + j_min);
+                },
+                tmp);
+            if (team.team_rank() == 0)
+            {
+                tmp *= alpha_;
+                Kokkos::atomic_add<Scalar>(&y_(i), tmp);
+            }
+        }
     }
-    for (IndexType i = 0; i < i_min; ++i)
-    {
-      Scalar tmp = 0.;
-      Kokkos::parallel_reduce(Kokkos::TeamThreadRange(team, nj), [=](int jj, Scalar &tmp_sum) {
-        tmp_sum += A_(jj + j_min, i)*x_(jj + j_min);
-      }, tmp);
-      if (team.team_rank() == 0) {
-        tmp *= alpha_;
-        Kokkos::atomic_add<Scalar>(&y_(i), tmp);
-      }
-    }
-  }
 
 private:
-  AlphaCoeffType alpha_;
-  typename AViewType::const_type A_;
-  typename XViewType::const_type x_;
-  BetaCoeffType beta_;
-  YViewType y_;
-  const IndexType n_c_;
+    AlphaCoeffType alpha_;
+    typename AViewType::const_type A_;
+    typename XViewType::const_type x_;
+    BetaCoeffType beta_;
+    YViewType y_;
+    const IndexType n_c_;
 };
 
 template <class Scalar,
@@ -191,30 +199,30 @@ void update_MP(
     typename VY::const_value_type &beta,
     const VY &y)
 {
-  using execution_space = typename VA::execution_space;
-  using IndexType = typename VA::size_type;
-  using policy_type = Kokkos::RangePolicy<execution_space, IndexType>;
+    using execution_space = typename VA::execution_space;
+    using IndexType = typename VA::size_type;
+    using policy_type = Kokkos::RangePolicy<execution_space, IndexType>;
 
-  // Get the dimensions
-  const size_t m = y.extent(0);
-  const size_t n = x.extent(0);
+    // Get the dimensions
+    const size_t m = y.extent(0);
+    const size_t n = x.extent(0);
 
 #ifdef KOKKOS_ENABLE_DEPRECATED_CODE
-  const size_t N = execution_space::thread_pool_size();
+    const size_t N = execution_space::thread_pool_size();
 #else
-  const size_t N = execution_space::impl_thread_pool_size();
+    const size_t N = execution_space::impl_thread_pool_size();
 #endif
-  const size_t m_c_star = Sacado_MP_Vector_GEMV_Tile_Size(sizeof(Scalar));
-  const size_t n_tiles_per_thread = ceil(((double)m) / (N * m_c_star));
-  const size_t m_c = ceil(((double)m) / (N * n_tiles_per_thread));
-  const size_t n_tiles = N * n_tiles_per_thread;
+    const size_t m_c_star = Sacado_MP_Vector_GEMV_Tile_Size(sizeof(Scalar));
+    const size_t n_tiles_per_thread = ceil(((double)m) / (N * m_c_star));
+    const size_t m_c = ceil(((double)m) / (N * n_tiles_per_thread));
+    const size_t n_tiles = N * n_tiles_per_thread;
 
-  policy_type range (0, n_tiles);
+    policy_type range(0, n_tiles);
 
-  using functor_type = updateF<VA, VX, VY, IndexType>;
-  functor_type functor (alpha, A, x, beta, y, m_c);
+    using functor_type = updateF<VA, VX, VY, IndexType>;
+    functor_type functor(alpha, A, x, beta, y, m_c);
 
-  Kokkos::parallel_for ("KokkosBlas::gemv[Update]", range, functor);
+    Kokkos::parallel_for("KokkosBlas::gemv[Update]", range, functor);
 }
 
 template <class Scalar,
@@ -228,40 +236,69 @@ void inner_products_MP(
     typename VY::const_value_type &beta,
     const VY &y)
 {
-  using execution_space = typename VA::execution_space;
-  using IndexType = typename VA::size_type;
-  using team_policy_type  = Kokkos::TeamPolicy<execution_space>;
-  using range_policy_type = Kokkos::RangePolicy<execution_space, IndexType>;
-  using member_type = typename team_policy_type::member_type;
+    // Get the dimensions
+    const size_t m = y.dimension_0();
+    const size_t n = x.dimension_0();
 
-  // Get the dimensions
-  const size_t m = y.extent(0);
-  const size_t n = x.extent(0);
-
-  const size_t team_size = STOKHOS_GEMV_TEAM_SIZE;
+    const size_t team_size = 4;
 
 #ifdef KOKKOS_ENABLE_DEPRECATED_CODE
-  const size_t N = execution_space::thread_pool_size();
+    const size_t N = execution_space::thread_pool_size();
 #else
-  const size_t N = execution_space::impl_thread_pool_size();
+    const size_t N = execution_space::impl_thread_pool_size();
 #endif
-  const size_t m_c_star = Sacado_MP_Vector_GEMV_Tile_Size(sizeof(Scalar));
-  const size_t n_tiles_per_thread = ceil(((double)n) / (N * m_c_star));
-  const size_t m_c = ceil(((double)n) / (N * n_tiles_per_thread));
-  const size_t n_per_tile2 = m_c * team_size;
+    const size_t m_c_star = Sacado_MP_Vector_GEMV_Tile_Size(sizeof(Scalar));
+    const size_t n_tiles_per_thread = ceil(((double)n) / (N * m_c_star));
+    const size_t m_c = ceil(((double)n) / (N * n_tiles_per_thread));
+    const size_t n_per_tile2 = m_c * team_size;
 
-  const size_t n_i2 = ceil(((double)n) / n_per_tile2);
+    const size_t n_i2 = ceil(((double)n) / n_per_tile2);
+    using Kokkos::TeamThreadRange;
 
-  team_policy_type team(n_i2, team_size);  
+    Kokkos::TeamPolicy<> policy = Kokkos::TeamPolicy<>(n_i2, team_size);
+    typedef Kokkos::TeamPolicy<>::member_type member_type;
 
-  Kokkos::parallel_for(m, KOKKOS_LAMBDA(const int i) {
-    y(i) *= beta;
-  });
+    Kokkos::parallel_for(
+        m, KOKKOS_LAMBDA(const int i) {
+            y(i) *= beta;
+        });
 
-  using functor_type = innerF<VA, VX, VY, IndexType>;
-  functor_type functor (alpha, A, x, beta, y, n_per_tile2);
+    Kokkos::parallel_for(
+        policy, KOKKOS_LAMBDA(member_type team_member) {
+            const size_t j = team_member.league_rank();
+            const size_t j_min = n_per_tile2 * j;
+            const size_t nj = (j_min + n_per_tile2 > n) ? (n - j_min) : n_per_tile2;
+            const size_t i_min = j % m;
 
-  Kokkos::parallel_for ("KokkosBlas::gemv[InnerProducts]", team, functor);
+            for (size_t i = i_min; i < m; ++i)
+            {
+                Scalar tmp = 0.;
+                Kokkos::parallel_reduce(
+                    TeamThreadRange(team_member, nj), [=](int jj, Scalar &tmp_sum) {
+                        inner_product_kernel<Scalar>(&A(jj + j_min, i), &x(jj + j_min), &tmp_sum);
+                    },
+                    tmp);
+                if (team_member.team_rank() == 0)
+                {
+                    tmp *= alpha;
+                    Kokkos::atomic_add(&y(i), tmp);
+                }
+            }
+            for (size_t i = 0; i < i_min; ++i)
+            {
+                Scalar tmp = 0.;
+                Kokkos::parallel_reduce(
+                    TeamThreadRange(team_member, nj), [=](int jj, Scalar &tmp_sum) {
+                        inner_product_kernel<Scalar>(&A(jj + j_min, i), &x(jj + j_min), &tmp_sum);
+                    },
+                    tmp);
+                if (team_member.team_rank() == 0)
+                {
+                    tmp *= alpha;
+                    Kokkos::atomic_add(&y(i), tmp);
+                }
+            }
+        });
 }
 
 template <class Scalar,
@@ -275,16 +312,16 @@ void gemv_MP(const char trans[],
              typename VY::const_value_type &beta,
              const VY &y)
 {
-  // y := alpha*A*x + beta*y,
+    // y := alpha*A*x + beta*y,
 
-  static_assert(VA::rank == 2, "GEMM: A must have rank 2 (be a matrix).");
-  static_assert(VX::rank == 1, "GEMM: x must have rank 1 (be a vector).");
-  static_assert(VY::rank == 1, "GEMM: y must have rank 1 (be a vector).");
+    static_assert(VA::rank == 2, "GEMM: A must have rank 2 (be a matrix).");
+    static_assert(VX::rank == 1, "GEMM: x must have rank 1 (be a vector).");
+    static_assert(VY::rank == 1, "GEMM: y must have rank 1 (be a vector).");
 
-  if (trans[0] == 'n' || trans[0] == 'N')
-    update_MP<Scalar, VA, VX, VY>(alpha, A, x, beta, y);
-  else
-    inner_products_MP<Scalar, VA, VX, VY>(alpha, A, x, beta, y);
+    if (trans[0] == 'n' || trans[0] == 'N')
+        update_MP<Scalar, VA, VX, VY>(alpha, A, x, beta, y);
+    else
+        inner_products_MP<Scalar, VA, VX, VY>(alpha, A, x, beta, y);
 }
 
 namespace KokkosBlas
@@ -302,11 +339,11 @@ gemv(const char trans[],
      typename Kokkos::View<DY, PY...>::const_value_type &beta,
      const Kokkos::View<DY, PY...> &y)
 {
-  typedef typename Kokkos::View<DA, PA...>::value_type Scalar;
-  typedef Kokkos::View<DA, PA...> VA;
-  typedef Kokkos::View<DX, PX...> VX;
-  typedef Kokkos::View<DY, PY...> VY;
-  gemv_MP<Scalar, VA, VX, VY>(trans, alpha, A, x, beta, y);
+    typedef typename Kokkos::View<DA, PA...>::value_type Scalar;
+    typedef Kokkos::View<DA, PA...> VA;
+    typedef Kokkos::View<DX, PX...> VX;
+    typedef Kokkos::View<DY, PY...> VY;
+    gemv_MP<Scalar, VA, VX, VY>(trans, alpha, A, x, beta, y);
 }
 
 } // namespace KokkosBlas
