2606c6bf84951f62d5f2dddd12524335d96aa260
kliegeois <kimliegeois@ymail.com>
2020-10-05 14:41:29 +0200

    ROL: Solve bugs related to the update of the solution vector or the parameter vector

--- packages/rol/adapters/thyra/src/function/ROL_ThyraProductME_Constraint_SimOpt.hpp
+++ packages/rol/adapters/thyra/src/function/ROL_ThyraProductME_Constraint_SimOpt.hpp
@@ -45,6 +45,8 @@
 #ifndef ROL_THYRARODUCTME_EQUALITYCONSTRAINT_SIMOPT
 #define ROL_THYRARODUCTME_EQUALITYCONSTRAINT_SIMOPT
 
+#define HAVE_ROL_DEBUG
+
 #include "Thyra_ProductVectorBase.hpp"
 #include "Thyra_DefaultProductVectorSpace.hpp"
 #include "Thyra_SpmdVectorSpaceDefaultBase.hpp"
@@ -107,7 +109,8 @@ public:
     }
     else {
       const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
-      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      Ptr<Vector<Real>> unew = u.clone();
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(*unew);
       ThyraVector<Real>  & thyra_f = dynamic_cast<ThyraVector<Real>&>(c);
       Teuchos::RCP<const Thyra::ProductVectorBase<Real> > thyra_prodvec_p = Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<Real>>(thyra_p.getVector());
 
@@ -797,7 +800,8 @@ public:
 
     if(supports_deriv) { //use derivatives computed by model evaluator
       const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
-      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      Ptr<Vector<Real>> unew = u.clone();
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(*unew);
       const ThyraVector<Real>  & thyra_v = dynamic_cast<const ThyraVector<Real>&>(v);
       const ThyraVector<Real>  & thyra_w = dynamic_cast<const ThyraVector<Real>&>(w);
 
@@ -841,6 +845,7 @@ public:
       // Compute Newton quotient
       ahwv.axpy(-1.0,*jv);
       ahwv.scale(0.5/h);
+      this->update(u,z);
     }
   }
 
@@ -870,7 +875,8 @@ public:
     if(supports_deriv) {  //use derivatives computed by model evaluator
 
       const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
-      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      Ptr<Vector<Real>> unew = u.clone();
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(*unew);
       const ThyraVector<Real>  & thyra_v = dynamic_cast<const ThyraVector<Real>&>(v);
       const ThyraVector<Real>  & thyra_w = dynamic_cast<const ThyraVector<Real>&>(w);
 
@@ -918,6 +924,7 @@ public:
       // Compute Newton quotient
       ahwv.axpy(-1.0,*jv);
       ahwv.scale(0.5/h);
+      this->update(u,z);
     }
   }
 
@@ -946,7 +953,8 @@ public:
     if(supports_deriv) { //use derivatives computed by model evaluator
 
       const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
-      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      Ptr<Vector<Real>> unew = u.clone();
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(*unew);
       const ThyraVector<Real>  & thyra_v = dynamic_cast<const ThyraVector<Real>&>(v);
       const ThyraVector<Real>  & thyra_w = dynamic_cast<const ThyraVector<Real>&>(w);
 
@@ -1003,6 +1011,7 @@ public:
       // Compute Newton quotient
       ahwv.axpy(-1.0,*jv);
       ahwv.scale(0.5/h);
+      this->update(u,z);
     }
   }
 
@@ -1031,7 +1040,8 @@ public:
     if(supports_deriv) {  //use derivatives computed by model evaluator
 
       const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
-      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      Ptr<Vector<Real>> unew = u.clone();
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(*unew);
       const ThyraVector<Real>  & thyra_v = dynamic_cast<const ThyraVector<Real>&>(v);
       const ThyraVector<Real>  & thyra_w = dynamic_cast<const ThyraVector<Real>&>(w);
 
@@ -1096,6 +1106,7 @@ public:
       // Compute Newton quotient
       ahwv.axpy(-1.0,*jv);
       ahwv.scale(0.5/h);
+      this->update(u,z);
     }
   }
 
