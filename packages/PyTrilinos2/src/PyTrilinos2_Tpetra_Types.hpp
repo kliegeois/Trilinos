7b420e5506dcbc9908e5a4bcd84b6536d54b18ea
kliegeois <kimliegeois@ymail.com>
2022-09-21 21:10:08 -0600

    tmp

--- packages/PyTrilinos2/src/PyTrilinos2_Tpetra_Types.hpp
+++ packages/PyTrilinos2/src/PyTrilinos2_Tpetra_Types.hpp
@@ -1,20 +1,5 @@
-#include <KokkosCompat_ClassicNodeAPI_Wrapper.hpp>
-#include <Tpetra_DistObject.hpp>
-#include <Teuchos_SerialDenseMatrix.hpp>
 #include <TpetraCore_ETIHelperMacros.h>
 
 namespace Tpetra {
-    using longlong = long long;
-#if defined(HAVE_TPETRA_INST_SERIAL)
-    using Kokkos_Compat_KokkosSerialWrapperNode = Kokkos::Compat::KokkosDeviceWrapperNode<Kokkos::Serial, Kokkos::HostSpace>;
-#endif
-#if defined(HAVE_TPETRA_INST_PTHREAD)
-    using Kokkos_Compat_KokkosThreadsWrapperNode = Kokkos::Compat::KokkosDeviceWrapperNode<Kokkos::Threads>;
-#endif
-#if defined(HAVE_TPETRA_INST_OPENMP)
-    using Kokkos_Compat_KokkosOpenMPWrapperNode = Kokkos::Compat::KokkosDeviceWrapperNode<Kokkos::OpenMP>;
-#endif
-#if defined(HAVE_TPETRA_INST_CUDA)
-    using Kokkos_Compat_KokkosCudaWrapperNode = Kokkos::Compat::KokkosDeviceWrapperNode<Kokkos::Cuda>;
-#endif
+    TPETRA_ETI_MANGLING_TYPEDEFS()
 }
