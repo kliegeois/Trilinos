d6ea6a97ee13d6f517e1ff583ff2e1f1b03d0096
kliegeois <kimliegeois@ymail.com>
2022-11-22 18:59:39 -0700

    Upddate PyROL

--- packages/PyTrilinos2/src/PyTrilinos2_ROL_ETI.hpp
+++ packages/PyTrilinos2/src/PyTrilinos2_ROL_ETI.hpp
@@ -2,14 +2,33 @@
 #define PYTRILINOS2_ROL_ETI
 
 #include "ROL_Vector.hpp"
-//#include "ROL_Constraint.hpp"
+#include "ROL_Objective.hpp"
+#include "ROL_Constraint.hpp"
+#include "ROL_Solver.hpp"
+#include "ROL_Problem.hpp"
 
 #define BINDER_ROL_VECTOR(SCALAR) \
   template class Vector<SCALAR>;
 
+#define BINDER_ROL_OBJECTIVE(SCALAR) \
+  template class Objective<SCALAR>;
+
+#define BINDER_ROL_CONSTRAINT(SCALAR) \
+  template class Constraint<SCALAR>;
+
+#define BINDER_ROL_SOLVER(SCALAR) \
+  template class Solver<SCALAR>;
+
+#define BINDER_ROL_PROBLEM(SCALAR) \
+  template class Problem<SCALAR>;
+
 namespace ROL {
 
   BINDER_ROL_VECTOR(double)
+  BINDER_ROL_OBJECTIVE(double)
+  //BINDER_ROL_CONSTRAINT(double)
+  //BINDER_ROL_SOLVER(double)
+  //BINDER_ROL_PROBLEM(double)
 
 }
 
