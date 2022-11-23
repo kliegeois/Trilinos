4c3836e88aae048c070154ed9444f59d35c45b39
kliegeois <kimliegeois@ymail.com>
2022-11-22 18:24:17 -0700

    Upddate PyROL

--- packages/PyTrilinos2/examples/vector_ROL.py
+++ packages/PyTrilinos2/examples/vector_ROL.py
@@ -1,11 +1,14 @@
 from PyTrilinos2.PyTrilinos2 import ROL
 import numpy as np
 from numpy import linalg as LA
-print(ROL)
+
 
 class myVector(ROL.Vector_double_t):
-    def __init__(self, length=1, default_value=0.):
-        self.values = default_value*np.ones((length,))
+    def __init__(self, length=1, default_value=0., values=None):
+        if values is None:
+            self.values = default_value*np.ones((length,))
+        else:
+            self.values = values
         super().__init__()
     def plus(self, b):
         self.values += b.values
@@ -15,9 +18,15 @@ class myVector(ROL.Vector_double_t):
         return np.dot(self.values, b.values)
     def norm(self):
         return LA.norm(self.values)
-    def clone(self, b):
-        self.values = np.copy(b.values)
-
+    def clone(self):
+        return myVector(values=np.copy(self.values))
+    def axpy(self, scale_factor, x):
+        ax = x.clone()
+        ax.scale(scale_factor)
+        self.plus(ax)
+    def setScalar(self, new_value):
+        self.values[:] = new_value
+    # To implement: applyUnary, applyBinary, reduce, randomize * 3
 
 
 a = myVector(10, 1.)
@@ -27,6 +36,11 @@ print(a.norm())
 a.zero()
 print(a.norm())
 
-a.plus(b)
+a.axpy(1., b)
 print(a.norm())
 print(a.dot(b))
+
+b.setScalar(2.)
+a = b
+print(a.apply(b))
+print(b.norm())
