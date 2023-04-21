60b82aa802c0a60909a6426381bed416ad4016ed
kliegeois <kimliegeois@ymail.com>
2023-04-21 17:12:52 -0600

    tmp

--- packages/muelu/src/Transfers/Smoothed-Aggregation/MueLu_CoarseMapFactory_def.hpp
+++ packages/muelu/src/Transfers/Smoothed-Aggregation/MueLu_CoarseMapFactory_def.hpp
@@ -61,7 +61,6 @@
 #include "MueLu_CoarseMapFactory_decl.hpp"
 #include "MueLu_Level.hpp"
 #include "MueLu_Aggregates.hpp"
-#include "MueLu_Aggregates_kokkos.hpp"
 #include "MueLu_Monitor.hpp"
 
 namespace MueLu {
