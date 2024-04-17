dc9f37cdfd8d11e5b179ff3a94fa2ee43ac4a1f5
knliege <knliege@sandia.gov>
2024-04-17 09:05:04 -0600

    tmp

--- packages/tpetra/core/src/Tpetra_BlockCrsMatrix_Helpers_decl.hpp
+++ packages/tpetra/core/src/Tpetra_BlockCrsMatrix_Helpers_decl.hpp
@@ -110,7 +110,7 @@ namespace Tpetra {
   ///   - Point rows corresponding to a particular mesh node must be stored consecutively.
   template<class Scalar, class LO, class GO, class Node>
   Teuchos::RCP<BlockCrsMatrix<Scalar, LO, GO, Node>>
-  convertToBlockCrsMatrix(const Tpetra::CrsMatrix<Scalar, LO, GO, Node>& pointMatrix, const LO &blockSize);
+  convertToBlockCrsMatrix(const Tpetra::CrsMatrix<Scalar, LO, GO, Node>& pointMatrix, const LO &blockSize); //, bool use_local_ID=true
 
   /// \brief Fill all point entries in a logical block of a CrsMatrix with zeroes. This should be called
   /// before convertToBlockCrsMatrix if your Crs is not filled. Performed on host.
